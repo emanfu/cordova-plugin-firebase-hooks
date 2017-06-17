08474dd7d613d1d6d7ee18b368627482dcafeeb3
Eman Fu <emanfu@gmail.com>
2017-06-17 00:37:52 -0700

    Make firebase analytics work on Android.

--- hooks/android/copy-google-services.js
+++ hooks/android/copy-google-services.js
@@ -34,25 +34,32 @@ module.exports = function (context) {
     var paths = ["google-services.json", path.join(androidFolder, "assets/www/google-services.json")];
 
     for (var i = 0; i < paths.length; i++) {
+      var destPath = path.join(androidFolder, "google-services.json");
       if (fileExists(paths[i])) {
         // logMe("Found this file to write to Android: " + paths[i]);
         try {
           var contents = fs.readFileSync(paths[i]).toString();
-          fs.writeFileSync(path.join(androidFolder, "google-services.json"), contents);
+          fs.writeFileSync(destPath, contents);
 
           var json = JSON.parse(contents);
           var strings = fs.readFileSync(path.join(androidFolder, "res/values/strings.xml")).toString();
 
           // strip non-default value
-          strings = strings.replace(new RegExp('<string name="google_app_id">[^<]+?</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)<\/string>', "i"), '');
 
           // strip non-default value
-          strings = strings.replace(new RegExp('<string name="google_api_key">[^<]+?</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)<\/string>', "i"), '');
 
           // strip empty lines
           strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1');
 
-          fs.writeFileSync(path.join(androidFolder, "res/values/strings.xml"), strings);
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)<\/string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
+
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)<\/string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
+          fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+
         } catch(err) {
           logMe(err);
         }
